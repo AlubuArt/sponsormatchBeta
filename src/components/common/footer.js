046214e9e6b35b4be086de8df84afded8bfbbd40
AlubuArt <57637214+AlubuArt@users.noreply.github.com>
2021-01-30 08:28:10 +0100

    removed unused components

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -6,11 +6,12 @@ const Footer = props => {
         <div className="container-fluid">
             <div className="row">
                 <div className="col-md-6 footer-copyright">
-                    <p className="mb-0">{"Copyright 2018 © Endless All rights reserved."}</p>
+                    <p className="mb-0">{"Copyright 2021 © SponsorMatch."}</p>
                 </div>
                 <div className="col-md-6">
-                    <p className="pull-right mb-0">{"Hand crafted & made with"}
-                        <i className="fa fa-heart"></i>
+                    <p className="pull-right mb-0">{"Udviklet særligt til det danske foreningsliv"}
+                        
+                        <i className="fas fa-hand-holding-heart" ></i>
                     </p>
                 </div>
             </div>
