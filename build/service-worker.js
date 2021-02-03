a01d077476ed2d47759e65b5e19468897b0b1736
AlubuArt <57637214+AlubuArt@users.noreply.github.com>
2021-02-03 14:57:42 +0100

    deployed

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.282db232eec50f8757545ac3f3e7c749.js"
+  "/precache-manifest.009efd9827565104f3bcba130ab698fb.js"
 );
 
 self.addEventListener('message', (event) => {
