5418c127a66056b96563903e9cf0a619e2de47e7
AlubuArt <jc@jcvisueldesign.dk>
2021-03-05 10:57:57 +0100

    done for now

--- src/components/applications/sponsorer/sponsoratCard.js
+++ src/components/applications/sponsorer/sponsoratCard.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from "react";
+import {Row, Col, Button} from 'react-bootstrap';
+
 
 
   const SponsoratCard = (props) => {
@@ -6,12 +8,50 @@ import React, { Fragment } from "react";
 
     return (
         <Fragment>
-            <div>
-                <h2>{props.sponsorName}</h2>
-                <h3>{props.status}</h3>
+            < div className="container-fluid">
+              <div className="col-sm-12">
+                <Row className="card-sponsorat">
+                    <Col md="2" >
+                      <div >
+                    <p>{props.sponsorName}</p>
+                    </div>
+                    </Col>
+                    <Col md="1">
+                      <div>
+                      <p>{props.amount}</p>
+                      </div>
+                    </Col>
+
+                    <Col md="1">
+                     <div >
+                        <p>{props.status}</p>
+                        </div>
+                    </Col>
+                    <Col md="1">
+                      <div >
+                        <p>{props.startDate}</p>
+                        </div>  
+                    </Col>
+                    <Col sm="1">
+                      <div>
+                        <p>{props.endDate}</p>
+                      </div>
+                    </Col>
+
+                    <Col sm="6" className="sponsorat-buttons" >
+                      <Button className="sponsorat-button btn-danger " onClick={props.delete}>Slet</Button>
+                      <Button className="sponsorat-button btn-sponsormatchGreen" onClick={props.edit}>Redigér</Button>
+                      <Button className="sponsorat-button btn-sponsormatchGreen" onClick={props.open}>Se sponsorat</Button>
+                    </Col>
+
+                </Row>
+              </div>
             </div>
         </Fragment>
     )
   }
 
   export default SponsoratCard;
+
+
+ 
